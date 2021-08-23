26d7a408ae1add4d3f9e19486e55f6b82d9f7f2c
102real <reggae1@naver.com>
2021-08-23 22:57:23 +0900

    총알발사 모션 추가

--- Assets/02.Scripts/FireLaserGun.cs
+++ Assets/02.Scripts/FireLaserGun.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class FireLaserGun : MonoBehaviour
 {
+    public Animator gunAnimator;
     public void FireGun()
     {
+        //Access the animator on the gun model, trigger the
+        gunAnimator.SetTrigger("Fire");
+
         Debug.Log("น฿ป็~");
     }
 }
