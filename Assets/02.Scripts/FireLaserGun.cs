f0d8752145380ac703917201d317481d39fdec93
102real <reggae1@naver.com>
2021-08-24 00:53:37 +0900

    레이저발사 최적화

--- Assets/02.Scripts/FireLaserGun.cs
+++ Assets/02.Scripts/FireLaserGun.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class FireLaserGun : MonoBehaviour
 {
     public Animator gunAnimator;
+    public GameObject LaserBeamModel;
+    public Transform LaserSpawnPoint;
     public void FireGun()
     {
         //Access the animator on the gun model, trigger the
         gunAnimator.SetTrigger("Fire");
+
+        GameObject generatedLaserBeam = Instantiate(LaserBeamModel, LaserSpawnPoint.position, LaserSpawnPoint.rotation);
 
         Debug.Log("น฿ป็~");
     }
---
+++ Assets/LaserBeamMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamMove : MonoBehaviour
+{
+    public float thrust = 10f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = transform.forward * thrust;
+        Destroy(gameObject, 2f);
+    }
+}
